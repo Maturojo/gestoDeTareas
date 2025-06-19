569eb8449dad621df0d72efde38ed5a7e7142ff1
Maturojo <maturojo123mdq@gmail.com>
2025-06-19 13:18:26 -0300

    Ingrese la url de render en cada js para que puede ingresar correctamente en el servidor

--- gestor-tareas-backend/index.js
+++ gestor-tareas-backend/index.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const { MONGO_URI, PORT } = require('./config');
 
 const app = express();
 
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // Conexión a MongoDB
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(MONGO_URI)
     .then(() => console.log('🟢 Conectado a MongoDB'))
     .catch(err => console.error('🔴 Error conectando a MongoDB:', err));
 
@@ -19,7 +19,6 @@ app.use('/api/tasks', require('./routes/tasks'));
 app.use('/api/observaciones', require('./routes/observaciones'));
 
 // Inicio del servidor
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`🚀 Servidor escuchando en el puerto ${PORT}`);
 });
