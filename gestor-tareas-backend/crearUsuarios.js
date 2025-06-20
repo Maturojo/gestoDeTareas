ed8f97dacfeb17be0a5f48fb6eb11aebb0a16c08
Maturojo <maturojo123mdq@gmail.com>
2025-06-20 20:50:22 -0300

    Cree los usuarios para los empleados!

---
+++ gestor-tareas-backend/crearUsuarios.js
@@ -0,0 +1,37 @@
+    // gestor-tareas-backend/crearUsuarios.js
+    import mongoose from 'mongoose';
+    import dotenv from 'dotenv';
+    import bcrypt from 'bcryptjs';
+    import Usuario from './models/Usuario.js';
+
+    dotenv.config();
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('✅ Conectado a MongoDB');
+
+    const empleados = [
+    { usuario: 'matias', password: '1234', nombre: 'Matías', puesto: 'Whatsapp' },
+    { usuario: 'facundo', password: '1234', nombre: 'Facundo', puesto: 'Taller' },
+    { usuario: 'ariel',   password: '1234', nombre: 'Ariel',   puesto: 'Producción' },
+    { usuario: 'guillermo', password: '1234', nombre: 'Guillermo', puesto: 'Delivery' }
+    ];
+
+    for (const emp of empleados) {
+    const yaExiste = await Usuario.findOne({ usuario: emp.usuario });
+    if (yaExiste) {
+        console.log(`⏭ Usuario "${emp.usuario}" ya existe. Skipping...`);
+        continue;
+    }
+
+    const hash = await bcrypt.hash(emp.password, 10);
+    const nuevo = new Usuario({
+        usuario: emp.usuario,
+        passwordHash: hash,
+        nombre: emp.nombre,
+        puesto: emp.puesto
+    });
+
+    await nuevo.save();
+    console.log(`✅ Usuario "${emp.usuario}" creado`);
+    }
+
+    mongoose.disconnect();
